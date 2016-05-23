7f61dee0df2d66cb07ab0e1e7070a2328ad4e751
Kevin Reed <kntreed@gmail.com>
2016-05-22 23:51:30 -0400

    Update bootable-iso.sh

--- sh/bootable-iso.sh
+++ sh/bootable-iso.sh
@@ -1,5 +1,6 @@
 diskutil list
 sudo umount /dev/disk3s2
+# sudo diskutil unmountdisk force disk3s2
 sudo dd if=~/Desktop/Windows10_x64_EN-US.iso of=/dev/rdisk3s2 bs=1m
 diskutil eject /dev/(IDENTIFIER)
 
