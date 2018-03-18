705b883e089d7c3bd66d88c2b6ebd56a7be864a4
Kevin Reed <kntreed@gmail.com>
2018-03-18 15:00:05 -0400

    Update vm-internet.sh

--- sh/vm-internet.sh
+++ sh/vm-internet.sh
@@ -1,3 +1,5 @@
+# https://kb.vmware.com/s/article/1026510
+
 sudo /Applications/VMware\ Fusion.app/Contents/Library/vmnet-cli --configure
 sudo /Applications/VMware\ Fusion.app/Contents/Library/vmnet-cli --stop
 sudo /Applications/VMware\ Fusion.app/Contents/Library/vmnet-cli --start
