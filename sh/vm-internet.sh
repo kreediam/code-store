b6407173391aab9dda79870bf9668ad33ef7c122
Kevin Reed <kntreed@gmail.com>
2018-03-18 14:59:25 -0400

    Create vm-internet.sh

---
+++ sh/vm-internet.sh
@@ -0,0 +1,3 @@
+sudo /Applications/VMware\ Fusion.app/Contents/Library/vmnet-cli --configure
+sudo /Applications/VMware\ Fusion.app/Contents/Library/vmnet-cli --stop
+sudo /Applications/VMware\ Fusion.app/Contents/Library/vmnet-cli --start
