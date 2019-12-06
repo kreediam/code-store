954ffc02ecedb1fd447cbf3cf58d98ee0f013656
Kevin Reed <kntreed@gmail.com>
2019-12-06 15:55:44 -0500

    Create ListProviders.aspx.cs

---
+++ dotnet/ListProviders.aspx.cs
@@ -0,0 +1,48 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Telerik.Sitefinity.DynamicModules.Builder;
+using Telerik.Sitefinity.Multisite;
+using Telerik.Sitefinity.Services;
+
+
+namespace SitefinityWebApp
+{
+    public partial class ListProviders : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            var context = SystemManager.CurrentContext;
+
+            if (context is MultisiteContext multisiteContext)
+            {
+                var sites = multisiteContext.GetSites();
+                foreach (var site in sites)
+                {
+                    using (new SiteRegion(site))
+                    {
+                        var siteTypesAndProvides = this.GetProviders(site);
+                    }
+                }
+            }
+            elsea
+            {
+                var typesAndProviders = this.GetProviders(context.CurrentSite);
+            }
+        }
+
+        private IDictionary<string, string[]> GetProviders(ISite site)
+        {
+            var result = new Dictionary<string, string[]>();
+
+            var dynamicModuleNames = ModuleBuilderManager.GetActiveTypes().Select(t => t.ModuleName).Distinct();
+            foreach (var dynamicModule in dynamicModuleNames)
+            {
+                var typeProviders = site.GetProviders(dynamicModule);
+                result.Add(dynamicModule, typeProviders.Select(p => p.ProviderName).ToArray());
+            }
+
+            return result;
+        }
+    }
+}
