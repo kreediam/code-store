47145a25ebea42130bfee91ed0ac227f6997d25a
Kevin Reed <kntreed@gmail.com>
2022-01-10 14:43:01 -0500

    Update find-sitefinity-widgets.sql

--- sql/find-sitefinity-widgets.sql
+++ sql/find-sitefinity-widgets.sql
@@ -2,6 +2,15 @@ select pn.title_, od.caption_, *
 from sf_object_data od
 join sf_page_node pn on od.page_id = pn.content_id
 join sf_control_properties cp on cp.control_id = od.id
-where object_type like '%Telerik.Sitefinity.Mvc.Proxy.MvcControllerProxy%'
+where od.object_type like '%Telerik.Sitefinity.Mvc.Proxy.MvcControllerProxy%'
 and cp.nme = 'ControllerName'
-and od.caption_ like '%StockWidget%'
+--and od.caption_ like '%StockWidget%'
+
+
+select pn.title_, od.caption_, *
+from sf_object_data od
+join sf_page_templates pn on od.page_id = pn.id
+join sf_control_properties cp on cp.control_id = od.id
+where od.object_type like '%Telerik.Sitefinity.Mvc.Proxy.MvcControllerProxy%'
+and cp.nme = 'ControllerName'
+--and od.caption_ like '%StockWidget%'
