bc4317b271a2f8de7cd0aa51091726ee702f4e4a
Kevin Reed <kntreed@gmail.com>
2022-01-10 14:44:09 -0500

    Update find-sitefinity-widgets.sql

--- sql/find-sitefinity-widgets.sql
+++ sql/find-sitefinity-widgets.sql
@@ -1,4 +1,4 @@
-select pn.title_, od.caption_, *
+select pn.title_, od.caption_, cp.val, *
 from sf_object_data od
 join sf_page_node pn on od.page_id = pn.content_id
 join sf_control_properties cp on cp.control_id = od.id
@@ -7,7 +7,7 @@ and cp.nme = 'ControllerName'
 --and od.caption_ like '%StockWidget%'
 
 
-select pn.title_, od.caption_, *
+select pn.title_, od.caption_, cp.val,*
 from sf_object_data od
 join sf_page_templates pn on od.page_id = pn.id
 join sf_control_properties cp on cp.control_id = od.id
