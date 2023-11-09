09d8b45b964fa3c225a3b220aad3ec17f81c9289
Kevin Reed <kntreed@gmail.com>
2023-11-09 18:45:05 -0500

    Add files via upload

---
+++ sh/create-encryption-key.ps1
@@ -0,0 +1,9 @@
+﻿[Byte[]] $bytes = 1..32
+$rng = [System.Security.Cryptography.RNGCryptoServiceProvider]::new()
+$rng.getbytes($bytes)
+$sb = [System.Text.StringBuilder]::new()
+foreach($b in $bytes)
+{
+    $sb.Append($b.ToString("X2"))
+}
+echo $sb.ToString()
