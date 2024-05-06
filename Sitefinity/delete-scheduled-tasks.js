dabde855b074f96012e4ef8dc565e991f84f5679
Kevin Reed <kreed@springthrough.com>
2024-05-06 18:01:49 -0400

    Remove server variable. Add status variable.

--- Sitefinity/delete-scheduled-tasks.js
+++ Sitefinity/delete-scheduled-tasks.js
@@ -1,34 +1,32 @@
 (function () {
 
-async function start() {
-	const server = "https://xyz.azurewebsites.net";
-	
-	const take = 50;
-	const taskType = "Telerik.Sitefinity.RecycleBin.ScheduledTasks.PermanentDeleteTaskImpl";
-	//const taskType = "SitefinityWebApp.Tasks.WorkdaySyncTask";
-	
-	var response = await fetch(`${server}/restapi/sitefinity/scheduling/scheduled-tasks?format=json&orderBy=0&skip=0&take=${take}&searchTerm=&filterBy=4`);
-	var json = await response.json();
-	console.log(`Found ${json.Items.length}`);
-	
-	for (var i = 0; i < json.Items.length; i++) {
-		var item = json.Items[i];
-		if(item.Name === taskType){
-			console.log(`Item ${i+1} is ${taskType}. Deleting...`);
-			await fetch(`${server}/restapi/sitefinity/scheduling/scheduled-tasks?taskId=${item.TaskId}&operation=3&hash=${item.Hash}`, { "method": "PUT" });
-			await sleep(1000);
-		} else {
-			console.log(`Item ${i+1} is ${item.Name}. Skipping...`);
+	async function start() {
+		const take = 50;
+		const status = "2";
+		const taskType = "Telerik.Sitefinity.RecycleBin.ScheduledTasks.PermanentDeleteTaskImpl";
+		
+		var response = await fetch(`/restapi/sitefinity/scheduling/scheduled-tasks?format=json&orderBy=0&skip=0&take=${take}&searchTerm=&filterBy=${status}`);
+		var json = await response.json();
+		console.log(`Found ${json.Items.length}`);
+
+		for (var i = 0; i < json.Items.length; i++) {
+			var item = json.Items[i];
+			if (item.Name === taskType) {
+				console.log(`Item ${i + 1} is ${taskType}. Deleting...`);
+				await fetch(`/restapi/sitefinity/scheduling/scheduled-tasks?taskId=${item.TaskId}&operation=3&hash=${item.Hash}`, { "method": "PUT" });
+				await sleep(1000);
+			} else {
+				console.log(`Item ${i + 1} is ${item.Name}. Skipping...`);
+			}
 		}
+
+		console.log("Done");
 	}
-	
-	console.log("Done");
-}
 
-function sleep(ms) {
-	return new Promise(resolve => setTimeout(resolve, ms));
-}
+	function sleep(ms) {
+		return new Promise(resolve => setTimeout(resolve, ms));
+	}
 
-start();
+	start();
 
 })();
